d6a49fca1aa2bd93b4b464360ae8a9d73d40ac05
yyj11111111 <92639078+yyj11111111@users.noreply.github.com>
2026-06-29 14:12:20 +0800

    Add files via upload

---
+++ YAOBridge/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+setuptools.setup(
+    name="YAOBridge",
+    version="2.0.0",
+    author="kangLab yijiyang",
+    author_email="15623109189@163.com",
+    description="A chain tool for confident position lift between hg38 and yao",
+    url="",
+    install_requires=["intervaltree",],
+    python_requires='>=3.10',
+    packages=setuptools.find_packages(),
+include_package_data=True, # 建议开启
+    package_data={
+        # 表示在 src 包下的 data 目录中包含所有 .chain 文件
+        "src": ["data/*.chain"], 
+    },
+    entry_points={'console_scripts': ['YAOBridge = src.PB:main'], },
+)
